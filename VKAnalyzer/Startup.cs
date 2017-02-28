a1d3f0ac2357324175b50903b41c2a8059fc3dc2
BatonrainLocal <Batonrain@gmail.com>
2017-02-28 15:16:08 +0400

    new project

---
+++ VKAnalyzer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VKAnalyzer.Startup))]
+namespace VKAnalyzer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
