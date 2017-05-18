872c9000969c91447a00dd4adb98173169d1acba
BatonrainLocal <Batonrain@gmail.com>
2017-05-18 20:46:32 +0400

    steps and counts are changed in VK

--- VKAnalyzer/Controllers/VkController.cs
+++ VKAnalyzer/Controllers/VkController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using NLog;
 using VKAnalyzer.BusinessLogic.CohortAnalyser;
-using VKAnalyzer.BusinessLogic.CohortAnalyser.Models;
 using VKAnalyzer.Models.VKModels;
 using VKAnalyzer.Services;
 
